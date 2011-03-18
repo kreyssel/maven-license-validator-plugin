1c5fcde17a1a9d0f5d2d7c8db0fad0e624492f45
Tim Darby <tim.darby.uk@gmail.com>
2011-03-18 00:54:23 +0000

    getting some of the site stuff fixed and working

--- src/main/java/com/googlecode/maven/licensevalidator/LicenseValidateMojo.java
+++ src/main/java/com/googlecode/maven/licensevalidator/LicenseValidateMojo.java
@@ -1,5 +1,5 @@
 /**
- * opyright (C) 2011 tdarby <tim.darby.uk@googlemail.com>
+ * Copyright (C) 2011 tdarby <tim.darby.uk@googlemail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
